1f3730bf81223d645b4f3cbad3fa9a9a70d14e3b
Remco Tolsma <info@remcotolsma.nl>
2016-01-11 16:49:15 +0100

    Fixed class namespace.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,7 +8,7 @@
  * @author Remco Tolsma
  * @version 1.0.0
  */
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouw_ConfigTest extends PHPUnit_Framework_TestCase {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouwV3_ConfigTest extends PHPUnit_Framework_TestCase {
 	public function test_config() {
 		$config = new Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouwV3_Config();
 
