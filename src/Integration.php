e469c7b7ab77c30f0525e10626d987e1d03a9854
I <info@remcotolsma.nl>
2016-10-19 08:56:03 +0200

    Getting ready for version 1.0.5.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.4
+ * @version 1.0.5
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_AbnAmro_IDealZelfbouwV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
