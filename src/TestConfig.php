686a6837c7200f88b0fc18285a2eaee85cd6d557
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 11:56:10 +0100

    Happy 2018.

--- src/TestConfig.php
+++ src/TestConfig.php
@@ -3,7 +3,7 @@
 /**
  * Title: ABN AMRO iDEAL Zelfbouw v3 config test
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
--- tests/ConfigFactoryTest.php
+++ tests/ConfigFactoryTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: ABN AMRO - iDEAL Zelfbouw - v3 - Config factory test
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
